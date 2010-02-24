7a25261bcc2637929d040684e474644dcf8c11be
Olivier Guilyardi <olivier@samalyse.com>
2010-02-24 14:34:58 +0000

    minor api enhancement: add IValueAnalyzer.__str__()

--- api.py
+++ api.py
@@ -197,6 +197,10 @@ class IValueAnalyzer(IAnalyzer):
         """Return the final result of the analysis performed over the data passed by
         repeatedly calling process()"""
 
+    def __str__(self):
+        """Return a human readable string containing both result and unit 
+        ('5.30dB', '4.2s', etc...)"""
+
 class IEffect(IProcessor):
     """Effect processor interface"""
 
