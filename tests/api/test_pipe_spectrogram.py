9639c7618a71d5cc0e0c1f7c0cd5f6e261fda126
yomguy <yomguy@parisson.com>
2010-03-11 15:02:07 +0000

    cleanup grapher tests

--- tests/api/test_pipe_spectrogram.py
+++ tests/api/test_pipe_spectrogram.py
@@ -2,7 +2,6 @@
 from timeside.tests.api import examples
 from timeside.core import *
 from timeside.api import *
-import os.path
 
 use_gst = 1
 if use_gst:
@@ -11,17 +10,7 @@ else:
     from timeside.tests.api.examples import FileDecoder, WavEncoder
 
 image_file = './spectrogram.png'
-
-# mono
-#source = os.path.join(os.path.dirname(__file__), "../samples/sweep_source.wav")
-#source = os.path.join(os.path.dirname(__file__), "../samples/guitar.wav")
-#source = os.path.join(os.path.dirname(__file__), "../samples/sweep_source.mp3")
-
-# stereo
-source = os.path.join(os.path.dirname(__file__), "../samples/sweep.wav")
-#source = os.path.join(os.path.dirname(__file__), "../samples/guitar_stereo.wav")
-#source = os.path.join(os.path.dirname(__file__), "/home/momo/music/flac/Isabelle Aboulker/Mon imagier des instruments/16 - Isabelle Aboulker - 16 instru.flac")
-#source = os.path.join(os.path.dirname(__file__), "/home/momo/music/ogg/Emilie_Jolie/01 - Henri Salvador - Prologue.ogg")
+source = '../samples/sweep.wav'
 
 decoder  = FileDecoder(source)
 spectrogram = examples.Spectrogram(width=1024, height=256, output=image_file, bg_color=(0,0,0), color_scheme='default')
