83414e9ba6e3cc73d1d25c96f98088f629f2a8a4
yomguy <yomguy@parisson.com>
2010-04-26 10:51:02 +0000

    add new audio test files, looping waveform tests with audio test files

--- tests/api/test_pipe_waveform.py
+++ tests/api/test_pipe_waveform.py
@@ -2,24 +2,34 @@
 from timeside.tests.api import examples
 from timeside.core import *
 from timeside.api import *
-import os.path
+import os
 
 use_gst = 1
 if use_gst:
-    from timeside.tests.api.gstreamer import FileDecoder, WavEncoder
+    from timeside.tests.api.gstreamer import FileDecoder
 else:
-    from timeside.tests.api.examples import FileDecoder, WavEncoder
+    from timeside.tests.api.examples import FileDecoder
 
-image_file = './waveform.png'
-source = os.path.join(os.path.dirname(__file__), "../samples/sweep.wav")
+sample_dir = '../samples'
+img_dir = './img'
+if not os.path.exists(img_dir):
+    os.mkdir(img_dir)
 
-decoder  = FileDecoder(source)
-waveform = examples.Waveform(width=1024, height=256, output=image_file, bg_color=(0,0,0), color_scheme='default')
+test_dict = {'sweep.wav': 'waveform_wav.png',
+            'sweep.flac': 'waveform_flac.png',
+            'sweep.ogg': 'waveform_ogg.png',
+            'sweep.mp3': 'waveform_mp3.png',
+            }
 
-(decoder | waveform).run()
-
-print 'frames per pixel = ', waveform.graph.samples_per_pixel
-print "render waveform to: %s" % image_file
-waveform.render()
+for source, image in test_dict.iteritems():
+    source = os.path.join(os.path.dirname(__file__), sample_dir + os.sep + source)
+    image = img_dir + os.sep + image
+    print 'Test : decoder(%s) | waveform (%s)' % (source, image)
+    decoder  = FileDecoder(source)
+    waveform = examples.Waveform(width=1024, height=256, output=image, bg_color=(0,0,0), color_scheme='default')
+    (decoder | waveform).run()
+    print 'frames per pixel = ', waveform.graph.samples_per_pixel
+    print "render waveform to: %s" %  image
+    waveform.render()
 
 
