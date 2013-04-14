b5f72c22612746a9ea2cc496924552fe9df5ee36
Paul Brossier <piem@piem.org>
2013-04-14 18:42:13 -0500

    timeside/__init__.py: simplify import, add tests to make sure we can get processors

---
+++ tests/test_list_processors.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+
+from unit_timeside import *
+import timeside
+verbose = 0
+
+class TestListCoreProcessors(TestCase):
+    """ test get list of processors """
+
+    def testHasSomeDecoders(self):
+        "has some decoders"
+        import timeside.encoder
+        procs = timeside.core.processors(timeside.api.IEncoder)
+        self.assertNotEquals(len(procs), 0)
+
+    def testHasSomeEncoders(self):
+        "has some encoders"
+        import timeside.encoder
+        procs = timeside.core.processors(timeside.api.IEncoder)
+        self.assertNotEquals(len(procs), 0)
+
+    def testHasSomeAnalyzers(self):
+        "has some analyzers"
+        import timeside.analyzer
+        procs = timeside.core.processors(timeside.api.IAnalyzer)
+        self.assertNotEquals(len(procs), 0)
+
+    def testHasSomeGraphers(self):
+        "has some graphers"
+        import timeside.grapher
+        procs = timeside.core.processors(timeside.api.IGrapher)
+        self.assertNotEquals(len(procs), 0)
+
+class TestListProcessors(TestCase):
+    """ test get list of processors """
+
+    def testHasSomeDecoders(self):
+        "has some decoders"
+        procs = timeside.get_processors(timeside.api.IEncoder)
+        self.assertNotEquals(len(procs), 0)
+
+    def testHasSomeEncoders(self):
+        "has some encoders"
+        procs = timeside.get_processors(timeside.api.IEncoder)
+        self.assertNotEquals(len(procs), 0)
+
+    def testHasSomeAnalyzers(self):
+        "has some analyzers"
+        procs = timeside.get_processors(timeside.api.IAnalyzer)
+        self.assertNotEquals(len(procs), 0)
+
+    def testHasSomeGraphers(self):
+        "has some graphers"
+        procs = timeside.get_processors(timeside.api.IGrapher)
+        self.assertNotEquals(len(procs), 0)
+
+if __name__ == '__main__':
+    unittest.main(testRunner=TestRunner())
