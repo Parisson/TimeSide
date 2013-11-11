6d8c6a451cd680c21b8545c4eab20c46795a43fe
Guillaume Pellerin <yomguy@parisson.com>
2013-11-11 21:57:19 +0100

    0.5.1 again

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -26,7 +26,7 @@ sys.path.insert(0, os.path.abspath('../../'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.viewcode', 'sphinx.ext.autosummary', 'numpydoc', 'sphinx.ext.doctest', 'sphinx.ext.coverage']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.viewcode', 'sphinx.ext.autosummary', 'sphinx.ext.doctest', 'sphinx.ext.coverage']
 
 doctest_path = os.path.abspath('../../')
 
