96502552eac259e6f98324b300043a8aebb158b3
Thomas Fillon <thomas@parisson.com>
2014-04-22 08:49:34 +0200

    Install Coveralls through setup.py test_requires

--- setup.py
+++ setup.py
@@ -61,6 +61,6 @@ setup(
   include_package_data = True,
   zip_safe = False,
   scripts=['scripts/timeside-waveforms', 'scripts/timeside-launch'],
-  tests_require=['pytest'],
+  tests_require=['pytest', 'coveralls'],
   cmdclass = {'test': PyTest},
     )
