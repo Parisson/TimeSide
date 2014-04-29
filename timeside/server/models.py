fe4386c6292790158007fd7739525536b754c20b
Guillaume Pellerin <yomguy@parisson.com>
2014-04-29 11:28:09 +0200

    server: all field editable

--- timeside/server/models.py
+++ timeside/server/models.py
@@ -39,7 +39,7 @@ class BaseResource(models.Model):
 
     date_added = models.DateTimeField(_('date added'), auto_now_add=True)
     date_modified = models.DateTimeField(_('date modified'), auto_now=True, null=True)
-    uuid = models.CharField(_('uuid'), unique=True, blank=True, max_length=512, editable=False)
+    uuid = models.CharField(_('uuid'), unique=True, blank=True, max_length=512)
 
     class Meta(MetaCore):
         abstract = True
