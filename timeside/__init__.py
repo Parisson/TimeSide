0178fd071ee552c8455e28a7d2e37092d6b3d718
Guillaume Pellerin <yomguy@parisson.com>
2014-05-16 09:15:20 +0200

    fix some copyrights

--- timeside/__init__.py
+++ timeside/__init__.py
@@ -1,4 +1,25 @@
 # -*- coding: utf-8 -*-
+#
+# Copyright (c) 2014 Thomas Fillon <thomas@parisson.com>
+
+# This file is part of TimeSide.
+
+# TimeSide is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+
+# TimeSide is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with TimeSide.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+# Thomas Fillon <thomas@parisson.com>
+
 from __future__ import absolute_import
 
 from . import api
--- timeside/server/views.py
+++ timeside/server/views.py
@@ -1,4 +1,25 @@
 # -*- coding: utf-8 -*-
+#
+# Copyright (c) 2014 Parisson SARL
+# Copyright (c) 2014 Guillaume Pellerin <yomguy@parisson.com>
+
+# This file is part of TimeSide.
+
+# TimeSide is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+
+# TimeSide is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with TimeSide.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author : Guillaume Pellerin <yomguy@parisson.com>
+
 
 from django.views.generic import *
 from django.http import HttpResponse, HttpResponseRedirect
