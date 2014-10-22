2f1dba67f51660507b61d3ef018b3e80112ee044
Guillaume Pellerin <yomguy@parisson.com>
2014-10-22 17:19:25 +0200

    cleanup old sandbox

--- tests/sandbox/test_limsi_sad.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import timeside
-
-decoder  =  timeside.decoder.FileDecoder('/home/momo/music_local/test/sweep.wav')
-analyzer = timeside.analyzer.LimsiSad('etape')
-(decoder | analyzer).run()
-print analyzer.results()
