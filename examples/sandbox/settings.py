8bcdcc27a9501c85c934efb9dfc96cc90576e6ed
Guillaume Pellerin <yomguy@parisson.com>
2014-11-06 14:03:48 +0100

    fix sandbox db path

--- examples/sandbox/settings.py
+++ examples/sandbox/settings.py
@@ -18,7 +18,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'timeside.sql',                      # Or path to database file if using sqlite3.
+        'NAME': PROJECT_ROOT + os.sep + 'timeside.sql', # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
