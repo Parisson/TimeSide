3eb69b3c8ab206b2ff67f6dd6b8c214aecce0b62
Thomas Fillon <thomas@parisson.com>
2014-11-28 14:44:09 +0100

    server: sandbox settings database goes back to sqlite

--- examples/sandbox/settings.py
+++ examples/sandbox/settings.py
@@ -17,12 +17,12 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'timeside', # Or path to database file if using sqlite3.
-        'USER': 'docker',                      # Not used with sqlite3.
-        'PASSWORD': 'docker',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',  # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_ROOT, 'timeside.sql'),  # Or path to database file if using sqlite3.
+        'USER': '',      # Not used with sqlite3.
+        'PASSWORD': '',  # Not used with sqlite3.
+        'HOST': '',      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
