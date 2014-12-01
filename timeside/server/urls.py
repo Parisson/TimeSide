4eea0f280230cc296773511e4278b343efffbbaf
Thomas Fillon <thomas@parisson.com>
2014-12-01 16:15:19 +0100

    Server: add download methods for transcoded media (for api serilaizers and timeside.server app)
    Add a html5 audio player to the item page
    
    see:
    - for app : /item/1, /item/1/download/ogg, /item/1/download/mp3
    - for api Serializers : /results/1/audio/

--- timeside/server/urls.py
+++ timeside/server/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework import routers
 from timeside.server import views
+from timeside.server.utils import TS_ENCODERS_EXT
+
+EXPORT_EXT = "|".join(TS_ENCODERS_EXT.keys())
 
 admin.autodiscover()
 
@@ -22,8 +25,19 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(api_router.urls)),
     url(r'^$', views.IndexView.as_view(), name="timeside-index"),
+    # Items
+    # ex: /item/5/
+    url(r'^items/(?P<pk>\d+)/$', views.ItemDetail.as_view(),
+        name='timeside-item-detail'),
+    # Get transcoded audio
+    # Ex: /item/5/download/ogg
+    url(r'^items/(?P<pk>\d+)/download/(?P<extension>' + EXPORT_EXT + ')$',
+        views.ItemExport.as_view(), name="item-export"),
+    # Results
     url(r'^results/(?P<pk>.*)/json/$', views.ResultAnalyzerView.as_view(),
         name="timeside-result-json"),
     url(r'^results/(?P<pk>.*)/png/$', views.ResultGrapherView.as_view(),
         name="timeside-result-png"),
+    url(r'^results/(?P<pk>.*)/audio/$', views.ResultEncoderView.as_view(),
+        name="timeside-result-audio"),
     )
