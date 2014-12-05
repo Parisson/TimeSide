c34678f9e6bf423b2ae85ea622b1d4c07db0dfe4
Thomas Fillon <thomas@parisson.com>
2014-12-05 15:18:48 +0100

    Server: update admin forms

--- timeside/server/admin.py
+++ timeside/server/admin.py
@@ -2,11 +2,22 @@
 from django.contrib import admin
 from timeside.server.models import *
 
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['date_added', 'date_modified', 'status']
+    list_filter = ['date_modified', 'status']
+
+
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ['__unicode__', 'date_added', 'date_modified', 'status']
+    list_filter = ['date_modified', 'status']
+
+
+
 admin.site.register(Selection)
 admin.site.register(Item)
 admin.site.register(Experience)
 admin.site.register(Processor)
 admin.site.register(Preset)
-admin.site.register(Result)
-admin.site.register(Task)
-
+admin.site.register(Result, ResultAdmin)
+admin.site.register(Task, TaskAdmin)
