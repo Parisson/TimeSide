9b6477cb733a6888daf126806a5f045371e3dc67
Guillaume Pellerin <yomguy@parisson.com>
2015-03-09 15:55:40 +0100

    apply a gain=0.97 to sweep and sine test samples, try native http streaming on encoder response

--- timeside/core/tools/test_samples.py
+++ timeside/core/tools/test_samples.py
@@ -79,11 +79,10 @@ class NumpySrc:
 class SampleArray(object):
     """Base Class for generating a data sample array"""
 
-    def __init__(self, duration=10, samplerate=44100, gain=0.97):
+    def __init__(self, duration=10, samplerate=44100):
         self.samplerate = int(samplerate)
         self.num_samples = int(numpy.ceil(duration * self.samplerate))
         self.array = NotImplemented
-        self.gain = gain
 
     @property
     def time_samples(self):
@@ -117,11 +116,11 @@ class SampleArray(object):
 class SineArray(SampleArray):
     """Class for generating a Sine array"""
     def __init__(self, frequency=440, duration=10, samplerate=44100,
-                 channels=1):
+                 channels=1, gain=0.97):
         super(SineArray, self).__init__(duration=duration,
                                         samplerate=samplerate)
         self.frequency = frequency
-        self.array = numpy.sin((2 * numpy.pi * self.frequency *
+        self.array = gain*numpy.sin((2 * numpy.pi * self.frequency *
                                self.time_samples / self.samplerate))
         self.array.resize(self.num_samples, 1)
 
@@ -129,7 +128,7 @@ class SineArray(SampleArray):
 class SweepArray(SampleArray):
     """Class for generating a Sweep array"""
     def __init__(self, f0=20, f1=None, duration=10, samplerate=44100,
-                 method='logarithmic'):
+                 method='logarithmic', gain=0.97):
         super(SweepArray, self).__init__(duration=duration,
                                          samplerate=samplerate)
 
@@ -139,7 +138,7 @@ class SweepArray(SampleArray):
         else:
             self.f1 = f1 / samplerate
         self.method = method
-        self.array = scipy.signal.waveforms.chirp(t=self.time_samples,
+        self.array = gain*scipy.signal.waveforms.chirp(t=self.time_samples,
                                                   f0=self.f0,
                                                   t1=self.time_samples[-1],
                                                   f1=self.f1,
