b9e3e4704c1929fef5d1900751a1c6eaa894fb38
Guillaume Pellerin <yomguy@parisson.com>
2015-03-14 02:46:40 +0100

    increase sqlite db lock time

--- examples/sandbox/settings.py
+++ examples/sandbox/settings.py
@@ -22,6 +22,9 @@ DATABASES = {
         'PASSWORD': '',  # Not used with sqlite3.
         'HOST': '',      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',      # Set to empty string for default. Not used with sqlite3.
+        'OPTIONS': {
+            'timeout': 20,
+        }
     }
 }
 
