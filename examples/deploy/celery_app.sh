3397e74cab994de25b4b5669c5904bd5898695d6
Guillaume Pellerin <yomguy@parisson.com>
2015-03-19 12:35:17 +0100

    add a wait script for better composition sequencing

--- examples/deploy/celery_app.sh
+++ examples/deploy/celery_app.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
 # paths
-app_dir='/opt/TimeSide/'
-sandbox_dir='/home/timeside/'
-manage=$sandbox_dir'manage.py'
+app='/opt/TimeSide/'
+sandbox='/home/sandbox/'
+manage=$sandbox'manage.py'
 
-python $manage syncdb --noinput
-python $manage migrate --noinput
-python $manage collectstatic --noinput
-python $manage timeside-create-admin-user
+sh $app/examples/deploy/wait.sh
 
 # Starting celery worker with the --autoreload option will enable the worker to watch for file system changes
 # This is an experimental feature intended for use in development only
