a2ad9539759e6f6afa3fa25cee8a37d2e08a5751
Thomas Fillon <thomas@parisson.com>
2016-01-26 17:12:25 +0100

    Fix and clean-up start_app_base.sh

--- app/deploy/start_app.sh
+++ app/deploy/start_app.sh
@@ -7,19 +7,17 @@ source "$SCRIPT_DIR/start_app_base.sh"
 python $manage migrate --noinput
 python $manage collectstatic --noinput
 
-if [ ! -f $app/.init ]; then
-    python $manage timeside-create-admin-user
-    python $manage timeside-create-boilerplate
-    python $manage bower_install -- --allow-root
-    touch $app/.init
-fi
+python $manage timeside-create-admin-user
+python $manage timeside-create-boilerplate
+python $manage bower_install -- --allow-root
+
 
 # static files auto update
 watchmedo shell-command --patterns="*.js;*.css" --recursive \
     --command='python '$manage' collectstatic --noinput' $static &
 
 # app start
-if [ $1 = "--runserver" ]
+if [ $1 == "--runserver" ]
 then
     python $manage runserver_plus 0.0.0.0:8000
 else
