ca7446b4b002dbe2badaa60a3b6d9eebedea69e4
Thomas Fillon <thomas@parisson.com>
2016-02-04 11:26:23 +0100

    Add script for updating environment

--- app/update_environment.sh
+++ app/update_environment.sh
@@ -1,3 +1,10 @@
+# This script update the environment with up-to-date dependencies
+# from the specification provides by environment.yml
+# and pinned the packages version in environment-pinned.yml
+
+# Please test the environment before committing changes to environment-pinned.yml
+# `python setup.py test`
+
 cd /srv/src/timeside/
 conda env create --name app --force --file environment.yml
 #python setup.py test
