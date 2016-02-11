02639ba2792f27d43925e2bef2eaca250ca03b43
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2016-02-11 18:12:03 +0100

    mv static and bower packages to data volumes

--- app/sandbox/settings.py
+++ app/sandbox/settings.py
@@ -194,7 +194,7 @@ CELERY_ALWAYS_EAGER = env('CELERY_ALWAYS_EAGER') # If this is True, all tasks wi
 
 from celery_app import app
 
-BOWER_COMPONENTS_ROOT = project_root('bower')
+BOWER_COMPONENTS_ROOT = '/srv/bower'
 BOWER_PATH = '/usr/local/bin/bower'
 BOWER_INSTALLED_APPS = (
     'jquery',
