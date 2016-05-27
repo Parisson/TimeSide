b6266beb74a5cdaaf9c931a8921b30486b989e16
Eric Debeir <eric.debeir@quematech.fr>
2016-05-27 14:53:56 +0200

    (player/server connexion) first waveform loaded \o/

--- timeside/player/static/timeside2/app/scripts/modules/data/commands/get_waveform.js
+++ timeside/player/static/timeside2/app/scripts/modules/data/commands/get_waveform.js
@@ -8,11 +8,14 @@ function (A) {
   
 
   var success = function (res) {
-    
+    if (res && res.body)
+      return A._v.trigCfg('data.items.waveform','apiok',res.body);
+
+    return A._v.trigCfg('data.items.waveform','apierror');    
   };
 
   var error = function (res) {
-     
+     return A._v.trigCfg('data.items.waveform','apierror');    
   };
 
   return function (data) {
