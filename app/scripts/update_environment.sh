8a033807d0ad4e1b1259e331e10d8e07b9e6ec67
Thomas Fillon <thomas@parisson.com>
2016-09-01 15:41:54 +0200

    Update conda environment file name

--- app/scripts/update_environment.sh
+++ app/scripts/update_environment.sh
@@ -1,11 +1,11 @@
 # This script update the environment with up-to-date dependencies
-# from the specification provides by environment.yml
+# from the specification provides by conda-environment.yml
 # and pinned the packages version in environment-pinned.yml
 
 # Please test the environment before committing changes to environment-pinned.yml
 # `python setup.py test`
 
 cd /srv/src/timeside/
-conda env create --name app --force --file environment.yml
+conda env create --name app --force --file conda-environment.yml
 #python setup.py test
 conda env export --name app --file environment-pinned.yml
