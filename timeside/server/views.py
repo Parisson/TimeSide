c830a428ad97283eb1d3b11bce841e2f99d0037f
Thomas Fillon <thomas@parisson.com>
2017-01-06 18:00:20 +0100

    Server: fix queryset filtering for analysis track list

--- timeside/server/views.py
+++ timeside/server/views.py
@@ -221,7 +221,7 @@ class AnalysisTrackViewSet(UUIDViewSetMixin, viewsets.ModelViewSet):
         analysis_uuid = self.request.query_params.get('analysis', None)
         item_uuid = self.request.query_params.get('item', None)
         if analysis_uuid is not None:
-            queryset = queryset.filter(analysis__uuid___startswith=analysis_uuid)
+            queryset = queryset.filter(analysis__uuid__startswith=analysis_uuid)
         if item_uuid is not None:
             queryset = queryset.filter(item__uuid__startswith=item_uuid)
 
