57f30839a71dd9f83a577fb1e1530f73f9940472
Thomas Fillon <thomas@parisson.com>
2017-01-09 23:36:43 +0100

    Server: Add a grapher for the linear spectrogram from the corresponding analyzer

--- timeside/plugins/analyzer/spectrogram.py
+++ timeside/plugins/analyzer/spectrogram.py
@@ -145,6 +145,18 @@ class Spectrogram(Analyzer):
                                            self.samplerate() / self.fft_size)
         self.add_result(spectrogram)
 
+        
+# Generate Grapher for IRITSpeech4Hz analyzer
+from timeside.core.grapher import DisplayAnalyzer
+
+DisplayIritTempogram = DisplayAnalyzer.create(
+    analyzer=Spectrogram,
+    result_id='spectrogram_analyzer',
+    grapher_id='spectrogram',
+    grapher_name='Linear Spectrogram',
+    staging=False)
+
+
 
 if __name__ == "__main__":
     import doctest
