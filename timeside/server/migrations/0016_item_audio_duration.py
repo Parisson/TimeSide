e82e68baab955b473cb45709fddbb27f0658c9b9
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2018-11-14 16:03:33 +0100

    fix migration

--- timeside/server/migrations/0016_item_audio_duration.py
+++ timeside/server/migrations/0016_item_audio_duration.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('server', '0015_auto_20161214_1621'),
+        ('timeside_server', '0015_auto_20161214_1621'),
     ]
 
     operations = [
