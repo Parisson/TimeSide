3f35cc0008e78a8d6850b15a1f168e1ea2ad69ac
agrandry <antoine.grandry@ircam.fr>
2019-03-15 16:07:34 +0100

    POC workflow wasabi

---
+++ tests/test_workflow_WASABI.py
@@ -0,0 +1,57 @@
+import time
+import requests
+from coreapi import Client
+from coreapi.auth import TokenAuthentication
+
+# """ http post http://localhost:9000/timeside/api-token-auth/ username=admin password=admin """
+url = 'http://localhost:9000/timeside/api-token-auth/'
+auth={'username':'admin', 'password':'admin'}
+r = requests.post(url, data=auth)
+token=r.json()['token']
+
+#coreapi client with the right token
+auth = TokenAuthentication(
+    scheme='Token',
+    token=token
+)
+client = Client(auth=auth)
+
+#schema of the API
+schema = client.get('http://localhost:9000/timeside/api/schema/')
+
+#Item
+keys = ['api', 'items', 'create']
+youtube_provider = '/timeside/api/providers/042d0121-456a-4b7d-a993-f8b040f6fc9c/'
+external_youtube_uri = 'https://www.youtube.com/watch?v=sOnqjkJTMaA'
+params = {'title':'Thriller','description':'Music from Michael Jackson','external_uri': external_youtube_uri,'provider': youtube_provider}
+item = client.action(schema,keys,params)
+
+#Ajout dans la selection WASABI
+keys = ['api', 'selections', 'update']
+WASABI_Selection_uuid = '3c58f084-a16d-470a-afc3-4a2341c46e40'
+params = {'uuid':WASABI_Selection_uuid, 'items':['/timeside/api/items/' + item['uuid'] + '/']}
+selec = client.action(schema,keys,params)
+
+#Experience
+keys = ['api', 'experiences', 'create']
+params = {'title':'aubio_temporal', 'presets':['/timeside/api/presets/b6f0deab-e39f-4df3-b586-2c0554101a34/']}
+exp = client.action(schema,keys,params)
+exp_uuid = exp['uuid']
+
+#Task
+keys = ['api', 'tasks', 'create']
+params = {'item' : '/timeside/api/items/' + item['uuid'] + '/', 'experience': '/timeside/api/experiences/' + exp_uuid + '/','status':2} # 2  --> pending ???
+
+task = client.action(schema,keys,params)
+
+time.sleep(20)
+
+#Results
+keys = ['api', 'results', 'list']
+params = {'search' : item['uuid']}
+
+result = client.action(schema,keys,params)
+
+result_uuid = result[0]['uuid'] 
+
+print('http://localhost:9000/timeside/results/' + result_uuid + '/json/')
