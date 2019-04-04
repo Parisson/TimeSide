fb043341b875b7b154876c63ca740e45a98b1aaf
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2019-04-04 17:32:24 +0200

    add local xmljson

--- app/bin/app_base.sh
+++ app/bin/app_base.sh
@@ -24,7 +24,7 @@ gid='www-data'
 # pip uninstall -y mysql-python
 # pip install -U mysql-python django==1.10.8 djangorestframework==3.8.2 mysqlclient
 # pip install coreapi
-pip install youtube-dl
+pip install youtube-dl xmljson
 pip install django-filter==1.1.0 djangorestframework==3.7 django==1.10
 
 # pip install django-inspect
