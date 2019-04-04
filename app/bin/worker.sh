089c750e3045a5501682a842914359c481c8a892
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2019-04-04 12:34:32 +0200

    run worker as root ATM

--- app/bin/worker.sh
+++ app/bin/worker.sh
@@ -6,5 +6,7 @@ source "$SCRIPT_DIR/app_base.sh"
 # Starting celery worker with the --autoreload option will enable the worker to watch for file system changes
 # This is an experimental feature intended for use in development only
 # see http://celery.readthedocs.org/en/latest/userguide/workers.html#autoreloading
-python $manage celery worker -A worker --uid $uid --gid $gid
+
+# python $manage celery worker -A worker --uid $uid --gid $gid
+python $manage celery worker -A worker
 
