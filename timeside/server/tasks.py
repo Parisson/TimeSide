64303570e95d5744f7969be9a5c38823c9863aee
Guillaume Pellerin <yomguy@parisson.com>
2019-05-20 17:13:21 +0200

    gc.collect after run

--- timeside/server/tasks.py
+++ timeside/server/tasks.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import time
+import gc
 
 from celery import shared_task
 from celery.result import AsyncResult
@@ -31,7 +32,7 @@ def experience_run(exp_id, item_id):
     item = Item.objects.get(id=item_id)
     experience = Experience.objects.get(id=exp_id)
     item.run(experience)
-
+    gc.collect()
 
 @shared_task
 def task_monitor(task_id, results_id):
