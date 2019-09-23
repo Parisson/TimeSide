7d1560de550ee279ef56c7e915fee0d37b34a3eb
Antoine Grandry <antoine.grandry@ircam.fr>
2019-09-23 16:07:58 +0200

    add get_first_or_create method for UUID models and refactore dangerous recurcive function

--- timeside/server/utils.py
+++ timeside/server/utils.py
@@ -11,37 +11,31 @@ TS_ENCODERS_EXT = {encoder.file_extension(): encoder.id()
                    for encoder in TS_ENCODERS
                    if encoder.file_extension()}
 
-def get_or_run_proc_result(pid, item, parameters=None):
+def get_or_run_proc_result(pid, item, parameters={}):
 
     # Get or Create Processor
-    processor, created = Processor.objects.get_or_create(pid=pid)
+    processor, c = Processor.objects.get_or_create(pid=pid)
+
     # Get or Create Preset with processor
-    presets = Preset.objects.filter(processor=processor,
+    preset, c = Preset.get_first_or_create(processor=processor,
                                     parameters=json.dumps(parameters))
-    if presets:
-        preset = presets[0]
-    else:
-        preset = Preset(processor=processor,
-                        parameters=json.dumps(parameters))
-        preset.save()
-                
-    # preset, created = Preset.objects.get_or_create(processor=processor, parameters=parameters)
-    # Get Result with preset and item
-    try:
-        result = Result.objects.get(item=item, preset=preset)
-        if not os.path.exists(result.hdf5.path):
-            # Result exists but not file (may have been deleted)
-            result.delete()
-            return get_or_run_proc_result(pid, item, parameters)
-        # Result and file exist --> OK
-        return result
-    except Result.DoesNotExist:
-        # Result does not exist
-        # the corresponding task has to be created and run
-        task, created = Task.objects.get_or_create(experience=preset.get_single_experience(),
-                                                   selection=item.get_single_selection())
+
+    # Get or create Result with preset and item
+    result, created = Result.get_first_or_create(preset=preset, 
+                                   item=item)
+
+    while not created and (not result.hdf5 or not os.path.exists(result.hdf5.path)):
+        # Result exists but does not content any file or the path points a deleted File
+        result.delete()
+        result, created = Result.get_first_or_create(preset=preset, 
+                                   item=item)
+    
+    if created: 
+        task, c = Task.get_first_or_create(experience=preset.get_single_experience(),
+                                                        selection=item.get_single_selection())
         task.run(wait=True)
-    return  get_or_run_proc_result(pid, item, parameters)
-            #response = StreamingHttpResponse(streaming_content=stream_from_task(task),
-            #                                 content_type=mime_type)
-            #return response
+
+    return result
+    
+# TODO def get_or_run_proc_item(pid, version, item, prarameters={}):
+# don't run the task if results exist for a proc with its version
