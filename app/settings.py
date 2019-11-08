4c60b3d4fda6d96948b89b67f4374f88b3db5fa2
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2019-11-08 17:48:27 +0100

    Add player v2 build script, remove bower finder, fix js app config for local dev

--- app/settings.py
+++ app/settings.py
@@ -96,7 +96,7 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-    'djangobower.finders.BowerFinder',
+    # 'djangobower.finders.BowerFinder',
 )
 
 TEMPLATES = [
---
+++ bin/timeside-player-v2-build.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+npm install -g bower
+npm install -g grunt-cli
+apt install ruby-sass
+
+cd /srv/lib/timeside/timeside/player/static/timeside2
+
+bower install --allow-root
+grunt build --force
