470f27c6ddd077d8dd58d24f40633c9e409d2c19
Paul Brossier <piem@piem.org>
2019-11-26 16:07:46 +0100

    [core] fix typo in GObject import

--- timeside/core/tools/gstutils.py
+++ timeside/core/tools/gstutils.py
@@ -4,7 +4,7 @@ import gi
 gi.require_version('Gst', '1.0')
 gi.require_version('GLib', '2.0')
 gi.require_version('GObject', '2.0')
-from gi.repository import GLib, Gobject, Gst
+from gi.repository import GLib, GObject, Gst
 Gst.init(None)
 
 import threading
