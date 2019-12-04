883c8f6c5a824ec27750c97b0272a02d8107fa1c
Paul Brossier <piem@piem.org>
2019-12-04 13:24:03 +0100

    [test] split test_decoding.py in 2 files

---
+++ tests/test_decoding_all.py
@@ -0,0 +1,236 @@
+import unittest
+from unit_timeside import TestRunner
+from test_decoding import TestDecoding, samples
+
+class TestDecodingSegment(TestDecoding):
+
+    def setUp(self):
+        super(TestDecodingSegment, self).setUp()
+        self.start = 1
+        self.duration = 3
+        self.source_duration = self.duration
+
+    def testMp3(self):
+        "Test mp3 decoding"
+        super(TestDecodingSegment, self).testMp3()
+
+    def testWav(self):
+        "Test wav decoding"
+        super(TestDecodingSegment, self).testWav()
+
+    def testWavMono(self):
+        "Test mono wav decoding"
+        super(TestDecodingSegment, self).testWavMono()
+
+    def testWav32k(self):
+        "Test 32kHz wav decoding"
+        super(TestDecodingSegment, self).testWav32k()
+
+    @unittest.skip("Flac not supported until bug fix is GST Gnonlin")
+    def testFlac(self):
+        "Test flac decoding"
+
+    @unittest.skip("Ogg not supported until bug fix is GST Gnonlin")
+    def testOgg(self):
+        "Test ogg decoding"
+
+
+class TestDecodingSegmentDefaultStart(TestDecodingSegment):
+
+    def setUp(self):
+        super(TestDecodingSegmentDefaultStart, self).setUp()
+        self.duration = 1
+        self.source_duration = self.duration
+
+    def testMp3(self):
+        "Test mp3 decoding"
+        super(TestDecodingSegmentDefaultStart, self).testMp3()
+
+
+class TestDecodingSegmentDefaultDuration(TestDecodingSegment):
+
+    def setUp(self):
+        super(TestDecodingSegment, self).setUp()
+        self.start = 1
+        self.source_duration -= self.start
+
+    def testWav(self):
+        "Test wav decoding"
+        super(TestDecodingSegment, self).testWav()
+
+    def testWavMono(self):
+        "Test mono wav decoding"
+        super(TestDecodingSegment, self).testWavMono()
+
+    def testWav32k(self):
+        "Test 32kHz wav decoding"
+        super(TestDecodingSegment, self).testWav32k()
+
+    def testMp3(self):
+        "Test mp3 decoding"
+        super(TestDecodingSegment, self).testMp3()
+
+
+class TestDecodingSegmentBadParameters(unittest.TestCase):
+
+    def setUp(self):
+        self.source = samples["sweep.wav"]
+
+    def test_bad_start_value(self):
+        "Test decoding segment with start value exceeding the media duration"
+        decoder = FileDecoder(self.source, start=10)
+        pipe = ProcessPipe(decoder)
+        self.assertRaises(ValueError, pipe.run)
+
+    def test_bad_duration_value(self):
+        "Test decoding segment with a too large duration value argument"
+        decoder = FileDecoder(self.source, duration=10)
+        pipe = ProcessPipe(decoder)
+        self.assertRaises(ValueError, pipe.run)
+
+
+class TestDecodingStereo(TestDecoding):
+
+    def setUp(self):
+        super(TestDecodingStereo, self).setUp()
+        self.samplerate, self.channels, self.blocksize = None, 2, None
+
+
+class TestDecodingMonoUpsampling(TestDecoding):
+
+    def setUp(self):
+        super(TestDecodingMonoUpsampling, self).setUp()
+        self.samplerate, self.channels, self.blocksize = 48000, None, None
+
+    def testMp3(self):
+        "Test mp3 decoding"
+        super(TestDecodingMonoUpsampling, self).testMp3()
+
+    def testWav(self):
+        "Test wav decoding"
+        super(TestDecodingMonoUpsampling, self).testWav()
+
+    def testWavMono(self):
+        "Test mono wav decoding"
+        super(TestDecodingMonoUpsampling, self).testWavMono()
+
+    def testWav32k(self):
+        "Test 32kHz wav decoding"
+        super(TestDecodingMonoUpsampling, self).testWav32k()
+
+    def testFlac(self):
+        "Test flac decoding"
+        super(TestDecodingMonoUpsampling, self).testFlac()
+
+    def testOgg(self):
+        "Test ogg decoding"
+        super(TestDecodingMonoUpsampling, self).testOgg()
+
+
+class TestDecodingMonoDownsampling(TestDecoding):
+
+    def setUp(self):
+        super(TestDecodingMonoDownsampling, self).setUp()
+        self.samplerate, self.channels, self.blocksize = 16000, None, None
+
+    def testWav32k(self):
+        "Test 32kHz wav decoding"
+        super(TestDecodingMonoDownsampling, self).testWav32k()
+
+    def testOgg(self):
+        "Test ogg decoding"
+        super(TestDecodingMonoDownsampling, self).testOgg()
+
+    def testMp3(self):
+        "Test mp3 decoding"
+        super(TestDecodingMonoDownsampling, self).testMp3()
+
+
+class TestDecodingStereoDownsampling(TestDecoding):
+
+    def setUp(self):
+        super(TestDecodingStereoDownsampling, self).setUp()
+        self.samplerate, self.channels, self.blocksize = 32000, 2, None
+
+    def testWav32k(self):
+        "Test 32kHz wav decoding"
+        super(TestDecodingStereoDownsampling, self).testWav32k()
+
+    def testOgg(self):
+        "Test ogg decoding"
+        super(TestDecodingStereoDownsampling, self).testOgg()
+
+    def testMp3(self):
+        "Test mp3 decoding"
+        super(TestDecodingStereoDownsampling, self).testMp3()
+
+
+class TestDecodingStereoUpsampling(TestDecoding):
+
+    def setUp(self):
+        super(TestDecodingStereoUpsampling, self).setUp()
+        self.samplerate, self.channels, self.blocksize = 96000, 2, None
+
+    def testWav32k(self):
+        "Test 32kHz wav decoding"
+        super(TestDecodingStereoUpsampling, self).testWav32k()
+
+    def testOgg(self):
+        "Test ogg decoding"
+        super(TestDecodingStereoUpsampling, self).testOgg()
+
+    def testMp3(self):
+        "Test mp3 decoding"
+        super(TestDecodingStereoUpsampling, self).testMp3()
+
+
+class TestDecodingShortBlock(TestDecoding):
+
+    def setUp(self):
+        super(TestDecodingShortBlock, self).setUp()
+        self.samplerate, self.channels, self.blocksize = None, None, 256
+
+
+class TestDecodingLongBlock(TestDecoding):
+
+    def setUp(self):
+        super(TestDecodingLongBlock, self).setUp()
+        self.samplerate, self.channels, self.blocksize = None, None, 1024 * \
+            8 * 2
+
+
+class TestDecodingWrongFiles(unittest.TestCase):
+
+    "Test decoding features"
+
+    def testMissingFile(self):
+        "Test decoding missing file"
+        self.source = os.path.join(os.path.dirname(__file__),
+                                   "a_missing_file.wav")
+        self.assertRaises(IOError, FileDecoder, self.source)
+
+    def testDevNull(self):
+        "Test decoding dev null"
+        self.source = "/dev/null"
+        with self.assertRaises(IOError):
+            FileDecoder(self.source)
+
+    def testNoAudioStream(self):
+        "Test decoding file withouth audio stream"
+        self.source = __file__
+        with self.assertRaises(IOError):
+            FileDecoder(self.source)
+
+    def testEmptyFile(self):
+        "Test decoding empty file"
+        import tempfile
+        self.tmpfile = tempfile.NamedTemporaryFile(delete=True)
+        self.source = self.tmpfile.name
+        with self.assertRaises(IOError):
+            FileDecoder(self.source)
+
+        self.tmpfile.close()
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=TestRunner())
