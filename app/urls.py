bb10a1d28c6d75a0688d9ed146e1cd05b7e832ff
Antoine Grandry <antoine.grandry@ircam.fr>
2019-12-09 14:06:09 +0100

    [server] replace obsoleted dj 2.1 login/logout views and admin url

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import RedirectView
 import django.contrib.auth.urls
 # Uncomment the next two lines to enable the admin:
@@ -13,20 +13,19 @@ admin.autodiscover()
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/timeside/')),
     url(r'^timeside/', include('timeside.server.urls')),
-
-    # Examples:
-    # url(r'^$', 'server.views.home', name='home'),
-    # url(r'^server/', include('server.foo.urls')),
-
+    
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     # Default login page
-    url(r'^accounts/login/$', auth_views.login,
-        {'template_name': 'timeside/login.html'},
-        name='timeside-login'),
-    url(r'^accounts/logout/$', auth_views.logout, name='timeside-logout'),
+    url(r'^accounts/login/$',
+        LoginView.as_view(template_name='timeside/login.html'),
+        name='timeside-login'
+        ),
+    url(r'^accounts/logout/$', LogoutView.as_view(),
+        name='timeside-logout'
+        ),
     #url('^', include('django.contrib.auth.urls'))
 ]
