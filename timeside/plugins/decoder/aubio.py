6446b3f3ec850c4b2622a28c9c9c617d4934a498
Paul Brossier <piem@piem.org>
2019-12-17 16:56:00 +0100

    [plugins] add first aubio_decoder draft

---
+++ timeside/plugins/decoder/aubio.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+from timeside.core.decoder import Decoder, IDecoder, implements, interfacedoc
+from timeside.plugins.decoder.utils import get_sha1
+import aubio
+
+class AubioDecoder(Decoder):
+    """ File decoder based on aubio """
+    implements(IDecoder)
+
+    def __init__(self, uri, start=0, duration=None, sha1=None):
+        super().__init__(start=start, duration=duration)
+        self.uri = uri
+        # create a temporary source to get the original file duration
+        tmp_source = aubio.source(self.uri)
+        self.input_samplerate = tmp_source.samplerate
+        self.input_channels = tmp_source.channels
+
+        self.input_totalframes = tmp_source.duration
+        self.input_duration = self.input_totalframes / self.input_samplerate
+        self.uri_duration = self.input_duration
+
+        # FIXME
+        self.mimetype = 'audio/x-wav'
+        self.input_width = 8
+
+        if sha1 is not None:
+            self._sha1 = sha1
+        else:
+            self._sha1 = get_sha1(uri)
+
+    def setup(self, channels=None, samplerate=None, blocksize=None):
+        kwargs = {}
+        if channels is not None:
+            kwargs.update ({'channels': channels})
+        if samplerate is not None:
+            kwargs.update ({'samplerate': samplerate})
+        if samplerate is not None:
+            kwargs.update ({'hop_size': blocksize})
+        self.source = aubio.source(self.uri, **kwargs)
+
+        self.output_blocksize = self.source.hop_size
+        self.output_channels = self.source.channels
+        self.output_samplerate  = self.source.samplerate
+
+    @staticmethod
+    @interfacedoc
+    def id():
+        return "aubio_decoder"
+
+    @staticmethod
+    @interfacedoc
+    def version():
+        return "1.0"
+
+    @interfacedoc
+    def process(self):
+        frames, read = self.source.do_multi()
+        self.eod = (read < self.output_blocksize)
+        frames = frames[:, :read].T
+        return frames, self.eod
+
+    @interfacedoc
+    def mime_type(self):
+        return self.mimetype
+
+    @interfacedoc
+    def resolution(self):
+        return 0
+
+    @interfacedoc
+    def metadata(self):
+        return {}
