ced4499fb7a051bd0f0b2d50c597839d6b9f9a3c
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2019-12-20 16:48:18 +0100

    [app] remove module install from startup

--- app/bin/app_base.sh
+++ app/bin/app_base.sh
@@ -29,8 +29,8 @@ gid='www-data'
 #pip install youtube-dl xmljson coreapi psycopg2-binary
 #pip install django-filter==1.1.0 djangorestframework==3.8 django==1.10 librosa==0.6.3
 #npm install -g bower
-pip install librosa==0.7.0
-pip install -U youtube-dl
+#pip install librosa==0.7.0
+#pip install -U youtube-dl
 
 # pip install django-inspect
 
