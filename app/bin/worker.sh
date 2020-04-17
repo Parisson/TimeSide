f279ee8b81cf854331a5f8432b4aad312e0e13a6
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2020-04-17 12:39:04 +0200

    [app] add uid and gid options to the celery command (should fix #180) Signoff: Antoine Grandry and Guillaume Pellerin

--- app/bin/worker.sh
+++ app/bin/worker.sh
@@ -4,8 +4,8 @@ SCRIPT_DIR="$(dirname "$0")"
 source "$SCRIPT_DIR/app_base.sh"
 
 if [ $DEBUG = True ]; then
-    python3 manage.py timeside-celery-worker --loglevel $loglevel --logfile $worker_logfile
+    python3 manage.py timeside-celery-worker --loglevel $loglevel --logfile $worker_logfile --uid $uid --gid $gid
 else
-    celery -A worker worker --loglevel=$loglevel --logfile=$worker_logfile
+    celery -A worker worker --loglevel=$loglevel --logfile=$worker_logfile --uid $uid --gid $gid
 fi
 
