b884f76afd1348dbb49c6997946bda966cd7bb19
Antoine Grandry <antoine.grandry@ircam.fr>
2020-04-17 12:48:59 +0200

    [server] set media, app and celery logs ownerships to www-data for worker and app

--- app/bin/app.sh
+++ app/bin/app.sh
@@ -4,14 +4,14 @@ SCRIPT_DIR="$(dirname "$0")"
 source "$SCRIPT_DIR/app_base.sh"
 
 # waiting for db
-python3 $manage wait-for-db
+su $uid -g $gid -s /bin/bash -c "python3 $manage wait-for-db"
 
 # run migrations
-python3 $manage migrate --noinput
+su $uid -g $gid -s /bin/bash -c "python3 $manage migrate --noinput"
 
 # timeside setup
-python3 $manage timeside-create-admin-user
-python3 $manage timeside-create-boilerplate
+su $uid -g $gid -s /bin/bash -c "python3 $manage timeside-create-admin-user"
+su $uid -g $gid -s /bin/bash -c "python3 $manage timeside-create-boilerplate"
 
 # if [ $DEBUG = "False" ]; then
 #     python $manage update_index --workers $processes &
@@ -25,13 +25,11 @@ npm install --prefix /srv/app
 # app start
 if [ "$1" = "--runserver" ]
 then
-    python3 $manage runserver 0.0.0.0:8000
+    su $uid -g $gid -s /bin/bash -c "python3 $manage runserver 0.0.0.0:8000"
 else
     # static files auto update
-    python3 $manage collectstatic --noinput
+    su $uid -g $gid -s /bin/bash -c "python3 $manage collectstatic --noinput"
 
-    # fix media access rights
-    find $media -maxdepth 1 -path ${media}import -prune -o -type d -not -user www-data -exec chown www-data:www-data {} \;
 
     # watchmedo shell-command --patterns="*.js;*.css" --recursive \
     #     --command='python '$manage' collectstatic --noinput' $src &
