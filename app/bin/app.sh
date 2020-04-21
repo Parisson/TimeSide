b7d3bdac8ae3a41e72391a0692082c82b076cdb0
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2020-04-21 11:35:56 +0200

    [app] avoid multiple chown

--- app/bin/app.sh
+++ app/bin/app.sh
@@ -3,6 +3,10 @@
 SCRIPT_DIR="$(dirname "$0")"
 source "$SCRIPT_DIR/app_base.sh"
 
+# fix log access
+mkdir -p '/var/log/app/'
+chown -R $uid:$gid '/var/log/app/'
+
 # waiting for db
 su $uid -g $gid -s /bin/bash -c "python3 $manage wait-for-db"
 
@@ -17,19 +21,20 @@ su $uid -g $gid -s /bin/bash -c "python3 $manage timeside-create-boilerplate"
 #     python $manage update_index --workers $processes &
 # fi
 
-
 # NPM modules install
 npm install --prefix /srv/app
-
 
 # app start
 if [ "$1" = "--runserver" ]
 then
     su $uid -g $gid -s /bin/bash -c "python3 $manage runserver 0.0.0.0:8000"
 else
-    # static files auto update
-    su $uid -g $gid -s /bin/bash -c "python3 $manage collectstatic --noinput"
 
+    # fix static access
+    chown -R $uid:$gid '/srv/static'
+
+    # collect static files
+    su $uid -g $gid -s /bin/bash -c "python3 $manage collectstatic --noinput"
 
     # watchmedo shell-command --patterns="*.js;*.css" --recursive \
     #     --command='python '$manage' collectstatic --noinput' $src &
