9c47562540fe2fe647ec2354ac9d4f9e88244c24
Guillaume Pellerin <guillaume.pellerin@ircam.fr>
2020-06-22 15:07:41 +0200

    worker: use the auto-reloading timeside-celery-worker command only

--- app/bin/worker.sh
+++ app/bin/worker.sh
@@ -7,9 +7,5 @@ source "$SCRIPT_DIR/app_base.sh"
 mkdir -p '/var/log/celery/'
 chown -R $uid:$gid '/var/log/celery/'
 
-if [ $DEBUG = True ]; then
-    python3 manage.py timeside-celery-worker --loglevel $loglevel --logfile $worker_logfile --uid $uid --gid $gid
-else
-    celery -A worker worker --loglevel=$loglevel --logfile=$worker_logfile --uid=$uid --gid=$gid
-fi
+python3 manage.py timeside-celery-worker --loglevel $loglevel --logfile $worker_logfile --uid $uid --gid $gid
 
