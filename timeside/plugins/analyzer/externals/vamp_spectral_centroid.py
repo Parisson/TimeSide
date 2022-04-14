f425d6054796d1ee982fde66bc2a3635d1159a2b
Paul Brossier <piem@piem.org>
2022-04-14 16:26:30 +0200

    [analyzers] set VampSpectralCentroid blocksize to 1024 (see #279)

--- timeside/plugins/analyzer/externals/vamp_spectral_centroid.py
+++ timeside/plugins/analyzer/externals/vamp_spectral_centroid.py
@@ -45,6 +45,8 @@ class VampSpectralCentroid(VampAnalyzer):
     @store_parameters
     def __init__(self):
         super(VampSpectralCentroid, self).__init__()
+        self.input_blocksize = 1024
+        self.input_stepsize = self.input_blocksize
         # Define Vamp plugin key and output
         self.plugin_key = 'vamp-libxtract:spectral_centroid'
         self.plugin_output = 'spectral_centroid'
